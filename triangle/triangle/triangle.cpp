1a87b21d30f9f723f9b22cb14beb3c5cb52bcb9a
yisonPylkita <yisonPylkita@gmail.com>
2017-10-09 19:15:38 +0200

    WIP: draw triangle

--- triangle/triangle/triangle.cpp
+++ triangle/triangle/triangle.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+#include <GL/glew.h>
 #include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
 
+
+using namespace std::string_literals;
 
 // TODO: replace it with spdlog
 class Log {
@@ -16,6 +19,15 @@ public:
     }
 };
 
+namespace {
+    // An array of 3 vectors which represents 3 vertices
+    const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f,  -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+}
+
 int main_impl()
 {
     sf::ContextSettings opengl_context;
@@ -25,6 +37,22 @@ int main_impl()
     sf::Window window(sf::VideoMode(800, 600), "Triangle", sf::Style::Default, opengl_context);
     window.setVerticalSyncEnabled(true);
     window.setActive(true);
+
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+        throw std::runtime_error("Could not initialize glew - "s +
+                                 reinterpret_cast<const char *>(glewGetErrorString(err)));
+
+    // triangle setup
+    // This will identify our vertex buffer
+    GLuint vertexbuffer{};
+    GLuint VertexArrayID{};
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
     bool running = true;
     while (running) {
@@ -44,6 +72,11 @@ int main_impl()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // draw...
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
 
         // end the current frame (internally swaps the front and back buffers)
         window.display();
@@ -58,7 +91,6 @@ int main()
     try {
         ret_code = main_impl();
     } catch (const std::exception &ex) {
-        using namespace std::string_literals;
         Log::error("Unhandled std exception -> "s + ex.what());
     } catch (...) {
         Log::error("Unrecognized unhandled exception");
