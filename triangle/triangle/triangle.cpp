a7e80ce5d23570dacde40fa07badb9f4305e8e7e
yisonPylkita <yisonPylkita@gmail.com>
2017-10-09 18:27:17 +0200

    Ported to CMake and sfml

--- triangle/triangle/triangle.cpp
+++ triangle/triangle/triangle.cpp
@@ -1,130 +1,69 @@
 #include <iostream>
 #include <string>
-#include <codecvt>
-#include <functional>
-#include <thread>
-#include <chrono>
-#include <GLFW/glfw3.h>
-
-using namespace std::chrono_literals;
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
 
 
-namespace str {
-    std::string ws2s(const std::wstring &wout)
-    {
-        typedef std::codecvt_utf8<wchar_t> convert_type;
-        std::wstring_convert<convert_type, wchar_t> converter;
-        return converter.to_bytes(wout);
-    }
-
-    std::wstring s2ws(const std::string &str)
-    {
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-        return converter.from_bytes(str);
-    }
-}
-
-struct Log {
-    static void debug(const std::wstring &msg)
-    {
-#ifdef _DEBUG
-        std::wcout << msg << std::endl;
-#endif
-    }
-
-    static void info(const std::wstring &msg)
-    {
-        std::wcout << msg << std::endl;
-    }
-
-    static void error(const std::wstring &msg)
-    {
-        std::wcout << msg << std::endl;
-    }
-};
-
-/// <summary>Execute some code on 
-///
-class on_scope_leave_class
-{
+// TODO: replace it with spdlog
+class Log {
 public:
-    using callback = std::function<void()>;
-
-    on_scope_leave_class(const callback &on_destroy)
-    {
-        _on_destroy = on_destroy;
+    static void debug(const std::string &msg) {
+        std::cout << "[DEBUG]: " << msg << std::endl;
     }
 
-    // Disallow object move or copy
-    on_scope_leave_class(const on_scope_leave_class &) = delete;
-    on_scope_leave_class operator=(const on_scope_leave_class &) = delete;
-    on_scope_leave_class(on_scope_leave_class &&) = delete;
-    on_scope_leave_class operator=(on_scope_leave_class &&) = delete;
-
-    ~on_scope_leave_class()
-    {
-        _on_destroy();
+    static void error(const std::string &msg) {
+        std::cout << "[ERROR]: " << msg << std::endl;
     }
-
-private:
-    callback _on_destroy;
 };
 
-#define TOKENPASTE(x, y) x ## y
-#define TOKENPASTE2(x, y) TOKENPASTE(x, y)
-#define UNIQUE TOKENPASTE2(Unique_, __LINE__)
-
-#define on_scope_leave(callback) on_scope_leave_class UNIQUE(callback)
-
-int wmain_impl()
+int main_impl()
 {
-    if (!glfwInit()) {
-        Log::error(L"Could not initialize glfw library");
-        return EXIT_FAILURE;
-    }
+    sf::ContextSettings opengl_context;
+    opengl_context.majorVersion = 4;
+    opengl_context.minorVersion = 5;
+    opengl_context.depthBits = 24;
+    sf::Window window(sf::VideoMode(800, 600), "Triangle", sf::Style::Default, opengl_context);
+    window.setVerticalSyncEnabled(true);
+    window.setActive(true);
 
-    on_scope_leave([] {
-        glfwTerminate();
-    });
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL triangle", nullptr, nullptr);
-    if (!window) {
-        Log::error(L"Could not initialize window");
-        return EXIT_FAILURE;
-    }
-
-    glfwMakeContextCurrent(window);
-    while (!glfwWindowShouldClose(window)) {
-        // draw
-        glClear(GL_COLOR_CLEAR_VALUE | GL_DEPTH_BUFFER_BIT);
-        glfwSwapBuffers(window);
-
+    bool running = true;
+    while (running) {
         // handle events
-        glfwPollEvents();
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                // end the program
+                running = false;
+            } else if (event.type == sf::Event::Resized) {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, event.size.width, event.size.height);
+            }
+        }
 
-        // most basic frames/second management
-        std::this_thread::sleep_for(10ms);
+        // clear the buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw...
+
+        // end the current frame (internally swaps the front and back buffers)
+        window.display();
     }
 
     return EXIT_SUCCESS;
 }
 
-int wmain()
+int main()
 {
     int ret_code = EXIT_SUCCESS;
     try {
-        ret_code = wmain_impl();
+        ret_code = main_impl();
     } catch (const std::exception &ex) {
-        Log::error(L"Unhandled std exception -> " + str::s2ws(ex.what()));
+        using namespace std::string_literals;
+        Log::error("Unhandled std exception -> "s + ex.what());
     } catch (...) {
-        Log::error(L"Unrecognized unhandled examption");
+        Log::error("Unrecognized unhandled exception");
     }
 
-    Log::debug(L"Application exited with code: " + std::to_wstring(ret_code));
+    Log::debug("Application exited with code: " + std::to_string(ret_code));
     return ret_code;
 }
